fd43c98661068adb0278edf979fb01d82f6a808d
Hellostafen <50908065+Hellostafen@users.noreply.github.com>
2019-08-07 10:56:16 +0800

    Update 合并K个排序_链表.py

--- 线型表/合并K个排序_链表.py
+++ 线型表/合并K个排序_链表.py
@@ -8,14 +8,14 @@ class Solution(object):
     def mergeKLists(self, lists):
         
         self.nodes = []
-        head = point = ListNode(0)  # 创建一个首节点，point用于扫描
+        head = point = ListNode(0)   # 创建一个首节点，point用于扫描
         
-        for list in lists:          # 扫描所有值
+        for list in lists:           # 扫描所有值
             while list:
                 self.nodes.append(list.val)
                 list = list.next
                 
-        for val in sorted(self.nodes):
+        for val in sorted(self.nodes):  # 创建新链表
             point.next = ListNode(val)
             point = point.next
         return head.next

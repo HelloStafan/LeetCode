69a611cd70a40311f7d6a854d79ac27ef4994206
Hellostafen <50908065+Hellostafen@users.noreply.github.com>
2019-08-16 09:36:01 +0800

    Update Max_Depth.py

--- Binary Tree/Max_Depth.py
+++ Binary Tree/Max_Depth.py
@@ -1,1 +1,19 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
 
+
+# 递归的思想 
+# 求取某节点的最大深度，就是求其两个子节点的最大深度，依次类推
+class Solution:
+    def maxDepth(self, root):
+        if root==None:
+            return 0
+        else:
+            D_left = self.maxDepth(root.left)
+            D_right = self.maxDepth(root.right)
+            return max(D_left, D_right)+1
+        
